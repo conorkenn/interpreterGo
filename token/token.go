6195fedcea1ce3b94f775531e9020fb6f876ef59
Conor Kennedy <conor@conorkennedy.com>
2022-01-25 16:27:08 -0500

    basic lexer working as intended

--- token/token.go
+++ token/token.go
@@ -16,26 +16,47 @@ const (
 	INT   = "INT"
 
 	//operator
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	RT = ">"
 
 	//delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
 
-	LPAREN = ")"
-	RPAREN = "("
+	LPAREN = "("
+	RPAREN = ")"
 	LBRACE = "}"
 	RBRACE = "{"
+
+	//equality
+	EQ     = "=="
+	NOT_EQ = "!="
 
 	//keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
